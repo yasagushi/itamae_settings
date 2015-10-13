b6570ddd2b6a8e33ece3a7b1da540b0cd8683197
Yasuo Sasaki <y-sasaki@forcus.xyz>
2015-10-13 14:32:32 +0000

    initial commit

--- settings.rb
+++ settings.rb
@@ -29,7 +29,6 @@ remote_file '/etc/php.ini' do
   mode '644'
   owner 'root'
   group 'root'
-  notifies :reload, 'service[httpd]'
 end
 
 # mysqlの設定
@@ -37,4 +36,42 @@ remote_file '/etc/my.cnf' do
   mode '644'
   owner 'root'
   group 'root'
+end
+
+# mysqlのルート設定
+root_password = node['mysql']['users'][0]['user_password']
+execute "password set for root" do
+  command "mysql -u root -p#{root_password} -e \"UPDATE mysql.user SET Password=PASSWORD(\'#{root_password}\') WHERE User='root';\""
+  not_if "mysql -u root -e \"UPDATE mysql.user SET Password=PASSWORD(\'#{root_password}\') WHERE User='root';\""
+end
+
+# mysqlデータベース作成
+db_name = node['mysql']['db_name']
+execute "create database" do
+  command "mysql -u root -p#{root_password} -e \"CREATE DATABASE #{db_name}\""
+  not_if "mysql -u root -p#{root_password} -e \"SHOW DATABASES\" | grep #{db_name}"
+end
+
+# mysqlユーザ作成
+node['mysql']['users'].length.times do |i|
+  user_name = node['mysql']['users'][i]['user_name']
+  user_password = node['mysql']['users'][i]['user_password']
+  
+  if user_name == "root" then
+    execute "databases setting" do
+      command <<-EOL
+        mysql -u root -p#{user_password} -e "DELETE FROM mysql.user WHERE User='';"
+        mysql -u root -p#{user_password} -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1');"
+        mysql -u root -p#{user_password} -e "DROP DATABASE test;"
+        mysql -u root -p#{user_password} -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
+        mysql -u root -p#{user_password} -e "FLUSH PRIVILEGES;"
+      EOL
+    end
+  else
+    execute "create users" do
+      command <<-EOL
+        mysql -u root -p#{root_password} -e "GRANT ALL ON #{db_name}.* TO \'#{user_name}\'@localhost IDENTIFIED BY \'#{user_password}\';"
+      EOL
+    end
+  end
 end
